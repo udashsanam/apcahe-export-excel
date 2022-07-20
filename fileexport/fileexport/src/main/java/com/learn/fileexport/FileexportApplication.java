ec7c7583c10d35f4b89da8ece18bfb3f93b73cc8
sanam udash <udashsanam@gmail.com>
2022-07-20 15:31:36 +0545

    add: initial project

---
+++ fileexport/fileexport/src/main/java/com/learn/fileexport/FileexportApplication.java
@@ -0,0 +1,13 @@
+package com.learn.fileexport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileexportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileexportApplication.class, args);
+	}
+
+}
---
+++ fileexport/fileexport/src/test/java/com/learn/fileexport/FileexportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.fileexport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileexportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
