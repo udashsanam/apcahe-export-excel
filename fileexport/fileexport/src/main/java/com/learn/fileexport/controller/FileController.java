ba18c8d17f646fa3f3193d48e5aaa0963686c3d9
sanam udash <udashsanam@gmail.com>
2022-07-21 12:15:22 +0545

    add: adding csv file data to database

--- fileexport/fileexport/src/main/java/com/learn/fileexport/controller/FileController.java
+++ fileexport/fileexport/src/main/java/com/learn/fileexport/controller/FileController.java
@@ -1,8 +1,12 @@
 package com.learn.fileexport.controller;
 
 
+import com.learn.fileexport.model.StudentEntity;
+import com.learn.fileexport.repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,14 +21,18 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/file")
+@Slf4j
 public class FileController {
 
 
+        private StudentRepository studentRepository;
 
+        @Autowired
+    public FileController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
-
-
-        // uploading file as multipart file by the use
+    // uploading file as multipart file by the use
     @PostMapping()
     public Map<Integer, List<String>> uploadFile(@RequestParam MultipartFile file){
 
@@ -84,6 +92,18 @@ public class FileController {
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        try {
+
+            for (int i = 0; i < data.size(); i++) {
+                List<String>  students = data.get(i);
+                studentRepository.save(new StudentEntity(students.get(0), students.get(1)));
+            }
+
+        } catch (Exception ex){
+
+            log.error(" Error while adding student ");
         }
 
 
